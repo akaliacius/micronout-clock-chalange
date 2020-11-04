24df31e3270b6f3cf71bffb28da00c7bdf5a4752
andrius.kaliacius <andrius.kaliacius@smarsh.com>
2020-11-04 17:56:57 +0000

    initial commit

---
+++ src/main/java/com/andrius/clock/Application.java
@@ -0,0 +1,10 @@
+package com.andrius.clock;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
