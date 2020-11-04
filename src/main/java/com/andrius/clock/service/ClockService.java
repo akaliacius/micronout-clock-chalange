f20f592e001d669a5849673ecf7b6c539c9dd22b
andrius.kaliacius <andrius.kaliacius@smarsh.com>
2020-11-04 21:45:19 +0000

    service

---
+++ src/main/java/com/andrius/clock/service/ClockService.java
@@ -0,0 +1,41 @@
+package com.andrius.clock.service;
+
+import com.andrius.clock.service.errors.DuplicateException;
+import com.andrius.clock.service.errors.NonExisitingRegistrationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Singleton;
+import java.util.List;
+
+@Singleton
+public class ClockService {
+    private static final Logger LOG = LoggerFactory.getLogger(ClockService.class);
+    private final DataStorage dataStorage;
+    private final ClockJob clockJob;
+
+    public ClockService(DataStorage dataStorage, ClockJob clockJob) {
+        this.dataStorage = dataStorage;
+        this.clockJob = clockJob;
+    }
+
+    public void register(String endpoint){
+        if(!dataStorage.insertEndpoint(endpoint)){
+            throw new DuplicateException(endpoint);
+        }
+    }
+
+    public void deregister(String endpoint){
+        if(!dataStorage.removeEndpoint(endpoint)){
+            throw new NonExisitingRegistrationException(endpoint);
+        }
+    }
+
+    public List<String> list(){
+        return dataStorage.listAll();
+    }
+
+    public void update(int seconds){
+        clockJob.update(seconds);
+    }
+}
