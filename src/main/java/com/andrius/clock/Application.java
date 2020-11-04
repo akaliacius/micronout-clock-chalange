45cfae666444a2166bf7417278d9d54246c94631
andrius.kaliacius <andrius.kaliacius@smarsh.com>
2020-11-04 21:49:14 +0000

    modified micronout application to run background job

--- src/main/java/com/andrius/clock/Application.java
+++ src/main/java/com/andrius/clock/Application.java
@@ -1,10 +1,26 @@
 package com.andrius.clock;
 
+import com.andrius.clock.service.ClockJob;
+import io.micronaut.context.event.ApplicationEventListener;
 import io.micronaut.runtime.Micronaut;
+import io.micronaut.runtime.server.event.ServerStartupEvent;
 
-public class Application {
+import javax.inject.Singleton;
+
+@Singleton
+public class Application implements ApplicationEventListener<ServerStartupEvent> {
+    private final ClockJob clockJob;
+
+    public Application(ClockJob clockJob) {
+        this.clockJob = clockJob;
+    }
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
+    }
+
+    @Override
+    public void onApplicationEvent(ServerStartupEvent event) {
+        clockJob.run();
     }
 }
